92289b100c2958fc6cb528baf351dd74d70e9f2e
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2025-01-14 17:50:27 +0000

    Add files via upload

---
+++ clean_up_code.py
@@ -0,0 +1,34 @@
+import csv
+import pandas as pd
+
+
+DATA_URL = "https://raw.githubusercontent.com/owid/monkeypox/refs/heads/main/owid-monkeypox-data.csv"
+df = pd.read_csv(DATA_URL)
+# Filter data for the United States
+spain_df = df[df['iso_code'] == 'ESP']
+
+DATA_URL = "https://raw.githubusercontent.com/owid/monkeypox/refs/heads/main/owid-monkeypox-data.csv"
+df = pd.read_csv(DATA_URL)
+# Filter data for the United States
+afr_df = df[df['iso_code'] == 'ZAF']
+
+#main columns
+columns_of_intrest = ['location','total_cases','total_deaths','date']
+afr_df=afr_df[columns_of_intrest]
+spain_df= spain_df[columns_of_intrest]
+#cleans the data 
+spain_df = spain_df.dropna()
+afr_df = afr_df.dropna()
+
+#combines both datasets
+combined_df = pd.concat([spain_df, afr_df])
+
+#adds the combind datasets to a csv file
+combined_df.to_csv("cleaned_up_data", index=False)
+
+file =open("cleaned_up_data.csv","r")
+dataIn = file.read()
+myList = dataIn.split(",")
+print(type(myList))
+
+
