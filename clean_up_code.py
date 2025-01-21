eae62350ea0d78c27edb5b27d4d98334aa695754
imogenemslt <144333984+imogenemslt@users.noreply.github.com>
2025-01-21 17:16:19 +0000

    Update clean_up_code.py

--- clean_up_code.py
+++ clean_up_code.py
@@ -4,42 +4,45 @@ import pandas as pd
 
 DATA_URL = "https://raw.githubusercontent.com/owid/monkeypox/refs/heads/main/owid-monkeypox-data.csv"
 df = pd.read_csv(DATA_URL)
-# Filter data for the United States
+# Filter data for the spain.
 spain_df = df[df['iso_code'] == 'ESP']
 
 DATA_URL = "https://raw.githubusercontent.com/owid/monkeypox/refs/heads/main/owid-monkeypox-data.csv"
 df = pd.read_csv(DATA_URL)
-# Filter data for the United States
-afr_df = df[df['iso_code'] == 'ZAF']
+# Filter data for the United kigdom
+uk_df = df[df['iso_code'] == 'GBR']
 
 #main columns
 columns_of_intrest = ['location','total_cases','total_deaths','date']
-afr_df=afr_df[columns_of_intrest]
+uk_df=uk_df[columns_of_intrest]
 spain_df= spain_df[columns_of_intrest]
 #cleans the data 
 spain_df = spain_df.dropna()
-afr_df = afr_df.dropna()
+uk_df = uk_df.dropna()
 
 #combines both datasets
-combined_df = pd.concat([spain_df, afr_df])
+combined_df = pd.concat([spain_df, uk_df])
 
 #adds the combind datasets to a csv file
-combined_df.to_csv("cleaned_up_data", index=False)
+combined_df.to_csv("cleaned_up_data.csv", index=False)
 
-file =open("cleaned_up_data.csv","r")
-dataIn = file.read()
-myList = dataIn.split(",")
-print(type(myList))
-
-#sorting list
-file =open("cleaned_up_data.csv","r")
+#list comprehention coverting floats to ints
+file = open("cleaned_up_data.csv","r")
 myList = file.read()
-myList.sort()
-print(myList)
-#list comprehention converting float into int
-file =open("cleaned_up_data.csv","r")
-dataIn = file.read()
-myList = dataIn.split(",")
-myList = [int(float(item)) for item in myList]
-file.close()
+reader = csv.DictReader(file)
+myList.split(",")
+death_data = [row['total_deaths']for row in reader ]
+processed_data =[int(value)for value in death_data if value.isdigit()]
+print(processed_data)
 
+case_data = [row['total_cases']for row in reader ]
+processed_data =[int(value)for value in case_data if value.isdigit()]
+print(processed_data)
+
+#finding the max cases
+target_row_index = 2
+row_values = df.iloc[target_row_index]
+max_value = row_values.max()
+print(max_value)
+
+file.close()
